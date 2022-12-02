431e4415e6037f025259ce28fcea1282d5be4019
pteacher <subfear@mail.ru>
2022-12-02 10:29:54 +0600

    Updates

---
+++ precache-manifest.d04dd2696160c7509d8e215ca61319bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7f5f3df43a3c919716c361487249d9",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "af1b4975e0fcfa8916bc",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "b61a36b4e4f55a361f47",
+    "url": "/emoji-search/static/css/main.bbbd6f71.chunk.css"
+  },
+  {
+    "revision": "af1b4975e0fcfa8916bc",
+    "url": "/emoji-search/static/js/2.b81e05a8.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.b81e05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61a36b4e4f55a361f47",
+    "url": "/emoji-search/static/js/main.eac9baea.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
